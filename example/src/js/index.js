f80c027d545086a86e2decd89cc9b3ef139fd8e9
marcosansoni <marco.sansoni.1995@gmail.com>
2021-03-03 06:44:06 +0100

    removed unused file and imports, updated .gitignore

--- example/src/js/index.js
+++
@@ -1,7 +0,0 @@
-import AnimatedLinksObjs from "./animatedLink";
-
-[...document.querySelectorAll('a.menu__link')].forEach((el) => {
-    const elPosition = [...el.parentNode.children].indexOf(el);
-    const fxObj = AnimatedLinksObjs[elPosition];
-    fxObj && new fxObj(el);
-});
